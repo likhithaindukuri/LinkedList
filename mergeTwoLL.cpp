fa3f497086799c74791885f50791b48fc9d5bf90
Indukuri Likhitha <likhithaindukuri07@gmail.com>
2023-08-24 21:24:50 +0530

    Practiced some LL qns

---
+++ mergeTwoLL.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==NULL)
+        {
+            return list2;
+        }
+        if(list2==NULL)
+        {
+            return list1;
+        }
+        if(list1->val>list2->val)
+        {
+            ListNode* temp=list1;
+            list1=list2;
+            list2=temp;
+        }
+        ListNode* res=list1;
+        while(list1!=NULL&&list2!=NULL)
+        {
+            ListNode* tmp=NULL;
+            while(list1!=NULL&&list1->val<=list2->val)
+            {
+                tmp=list1;
+                list1=list1->next;
+            }
+            tmp->next=list2;
+
+            ListNode* temp=list1;
+            list1=list2;
+            list2=temp;
+        }
+        return res;
+    }
+};
